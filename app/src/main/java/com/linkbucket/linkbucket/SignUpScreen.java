a2b1381871070273329aa7991d7e980ee83d6765
Akansh <akanshchoudhary79@gmail.com>
2021-01-15 22:55:27 +0530

    LinkBucket Version 1.0 Final Commit

--- app/src/main/java/com/linkbucket/linkbucket/SignUpScreen.java
+++ app/src/main/java/com/linkbucket/linkbucket/SignUpScreen.java
@@ -30,6 +30,7 @@ public class SignUpScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
         firebaseAuth = FirebaseAuth.getInstance();
